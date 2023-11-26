068d6814327cb14482acb51871048bf69c931dd7
tufanmulla <iamtufanmulla@gmail.com>
2023-11-27 04:22:41 +0530

    menuBar designed completed finally

--- js/headers.js
+++ js/headers.js
@@ -1,26 +1,22 @@
 // =============================================================================
 // Define All The Necessary Element First. Starts Are Here.
-// Clickable Button is #sshMenuBarButton
-let sshMenuBar = document.getElementById("sshMenuBarButton");
-// Open Then .sshMenu When Click On The #sshMenuBarButton
+let sshMenuBarButton = document.getElementById("sshMenuBarButton");
 let sshMenu = document.querySelector(".sshMenu");
 // Define All The Necessary Element First. Ends Are Here.
 // =============================================================================
 
 // =============================================================================
-// When Users Click On The #sshMenuBarButton Element Then Show The Menu Links SideBar Which is .sshMenu. Starts Are Here.
-
-sshMenu.style.display = "none";
-
-// Initially Hide The .sshMenu
-
-sshMenuBar.addEventListener("click", function () {
-  if (sshMenu.style.display === "none") {
-    sshMenu.style.display = "block";
-    sshMenu.style.transition = "all 0.3s";
+// When Users Click On The #sshMenuBarButton Element Then Add .sshMenuToggle At .sshMenu Then Toggling The .sshMenu. Starts Are Here
+sshMenuBarButton.addEventListener("click", function () {
+  if (
+    sshMenu.classList.contains("sshMenuToggle")
+  ) {
+    sshMenu.classList.remove("sshMenuToggle");
+    sshMenu.classList.remove("sshMennuToggleZoomAniamation");
   } else {
-    sshMenu.style.display = "none";
+    sshMenu.classList.add("sshMenuToggle");
+    sshMenu.classList.add("sshMennuToggleZoomAniamation");
   }
 });
-// When UsersClick On The #sshMenuBarButton Element Then Show The Menu Links SideBar. Which is .sshMenu. Ends Are Here.
+// When Users Click On The #sshMenuBarButton Element Then Add .sshMenuToggle At .sshMenu Then Toggling The .sshMenu. Engs Are Here
 // =============================================================================
