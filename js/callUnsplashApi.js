fd6156e85ac2e124c43457966962b15310f2dc8f
tufanmulla <iamtufanmulla@gmail.com>
2023-11-28 20:15:24 +0530

    fetching images from unsplash with simplicity

--- js/callUnsplashApi.js
+++ js/callUnsplashApi.js
@@ -1,5 +1,56 @@
-const apiKey = "QS9d2PodO1ovl-TNkgj52sevLJQdyr7e5KBLO0EWEEw";
+// const apiKey = "";
+let page = 1; // Track the page number for infinite scrolling
 
+// Function to fetch and display images
+function fetchAndDisplayImages(searchTerm, page) {
+  fetch(
+    `https://api.unsplash.com/photos/random?client_id=${apiKey}&query=${searchTerm}&count=5&page=${page}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Extract the image URLs from the response
+      const imageUrls = data.map((photo) => photo.urls.regular);
+
+      // Display the images on the webpage
+      const horizontalDiv = document.querySelector(".horizontal");
+      const verticalDiv = document.querySelector(".vertical");
+
+      imageUrls.forEach((imageUrl) => {
+        const horizontalImg = document.createElement("img");
+        const verticalImg = document.createElement("img");
+        horizontalImg.src = imageUrl;
+        verticalImg.src = imageUrl;
+        horizontalImg.alt = "Snap Sync Horizontal Images";
+        verticalImg.alt = "Snap Sync Vertical Images";
+        horizontalDiv.appendChild(horizontalImg);
+        verticalDiv.appendChild(verticalImg);
+      });
+    })
+    .catch((error) => console.error("Error fetching images:", error));
+}
+
+// Fetch and display images on page load
+document.addEventListener("DOMContentLoaded", function () {
+  const searchTerm = document.getElementById("search").value;
+  fetchAndDisplayImages(searchTerm, page);
+});
+
+// Event listener for the scroll event
+window.addEventListener("scroll", function () {
+  // Check if the user is near the bottom of the page
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+    // Increase the page number for the next set of images
+    page++;
+
+    // Get the search term from the input box
+    const searchTerm = document.getElementById("search").value;
+
+    // Fetch and display more images
+    fetchAndDisplayImages(searchTerm, page);
+  }
+});
+
+// Event listener for the form submission
 document
   .getElementById("searchForm")
   .addEventListener("submit", function (event) {
@@ -8,25 +59,12 @@ document
     // Get the search term from the input box
     const searchTerm = document.getElementById("search").value;
 
-    // Fetch images based on the search term
-    fetch(
-      `https://api.unsplash.com/photos/random?client_id=${apiKey}&query=${searchTerm}&count=5`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // Extract the image URLs from the response
-        const imageUrls = data.map((photo) => photo.urls.regular);
+    // Clear previous images
+    document.getElementById("imageContainer").innerHTML = "";
 
-        // Display the images on the webpage
-        const imageContainer = document.getElementById("imageContainer");
-        imageContainer.innerHTML = ""; // Clear previous images
+    // Reset the page number
+    page = 1;
 
-        imageUrls.forEach((imageUrl) => {
-          const img = document.createElement("img");
-          img.src = imageUrl;
-          img.alt = "Unsplash Image";
-          imageContainer.appendChild(img);
-        });
-      })
-      .catch((error) => console.error("Error fetching images:", error));
+    // Fetch and display images based on the new search term
+    fetchAndDisplayImages(searchTerm, page);
   });
