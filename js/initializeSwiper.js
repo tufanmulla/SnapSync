8894d10cfa884773446ec4a136501ee86ba0cfff
tufanmulla <iamtufanmulla@gmail.com>
2023-12-02 18:43:11 +0530

    added and fixed the category problem but need to modify something

---
+++ js/initializeSwiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop:true,
+  slidesPerView: 9,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
